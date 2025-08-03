a907692f1907d4effeb20d41f3c70737a9e60549
Esraa Hussien <67114479+esraahussie@users.noreply.github.com>
2025-08-04 02:40:47 +0300

    Add files via upload

---
+++ Uber.PLL/Controllers/RideController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Uber.PLL.Controllers
+{
+    public class RideController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

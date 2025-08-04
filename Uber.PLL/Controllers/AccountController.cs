8d23ebe0e7e9973bf768fadc60eda8bc260965fa
Esraa Hussien <67114479+esraahussie@users.noreply.github.com>
2025-08-05 00:22:51 +0300

    Add files via upload

---
+++ Uber.PLL/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Uber.PLL.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ChooseLoginType()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login(string role)
+        {
+            if (role == "Driver")
+                return View("~/Views/Driver/Login.cshtml");
+
+            if (role == "User")
+                return View("~/Views/User/Login.cshtml");
+
+            return NotFound();
+        }
+
+        public IActionResult ChooseRegisterType()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register(string role)
+        {
+            if (role == "Driver")
+                return View("~/Views/Driver/Register.cshtml");
+
+            if (role == "User")
+                return View("~/Views/User/Register.cshtml");
+
+            return NotFound();
+        }
+
+    }
+}

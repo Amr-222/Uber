ab5e3f94e848b3c0360486a4936bd71e0ca24160
Ibrahim <ibrahim.hamed05@eng-st.cu.edu.eg>
2025-08-08 21:12:07 +0300

    added editUser dto

---
+++ Uber.BLL/ModelVM/User/EditUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uber.BLL.ModelVM.User
+{
+    public class EditUser
+    {
+        public string Name { get; set; }
+        public DateTime Dateofbirth { get; set; }
+        public string? Imagepath { get; set; }
+        //public Location Address { get; set; } //To be changed
+    }
+}

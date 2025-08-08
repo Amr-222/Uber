49b6c2831af3480bbd7805540c8fab4bdc6fdb1a
Ibrahim <ibrahim.hamed05@eng-st.cu.edu.eg>
2025-08-08 20:22:21 +0300

    Temp files that have both the controller and view for the map

--- Uber.DAL/Entities/Ride.cs
+++ Uber.DAL/Entities/Ride.cs
@@ -14,11 +14,14 @@ namespace Uber.DAL.Entities
 
         public Location From { get; set; }
         public Location To { get; set; }
-
         public DateTime Date { get; set; } = DateTime.Now;
 
         public int Rate {  get; set; }
-
+        
+        public string DriverId { get; set; }
+        public Driver Driver { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
 
 
 

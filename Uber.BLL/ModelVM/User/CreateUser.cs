b7eca040cad605efeb37d4fb8da326b15ed6c3e3
Ibrahim <ibrahim.hamed05@eng-st.cu.edu.eg>
2025-08-12 02:31:50 +0300

    User Register and Login

--- Uber.BLL/ModelVM/User/CreateUser.cs
+++ Uber.BLL/ModelVM/User/CreateUser.cs
@@ -4,11 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Uber.DAL.Enums;
 
 namespace Uber.BLL.ModelVM.User
 {
     public class CreateUser
     {
-        public IFormFile? File { get; set; }
+        public string Name { get; set; }
+        public string Email { get;  set; }
+        public string Password { get;  set; }
+        public Gender Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
     }
 }

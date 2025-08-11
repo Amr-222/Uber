cb5da09ec98234534428b9b3f903add9ae9b4663
Amr-222 <144175308+Amr-222@users.noreply.github.com>
2025-08-12 02:29:47 +0300

    Register Front Edit

--- Uber.BLL/ModelVM/Driver/CreateDriver.cs
+++ Uber.BLL/ModelVM/Driver/CreateDriver.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Uber.DAL.Entities;
+using Uber.DAL.Enums;
 
 namespace Uber.BLL.ModelVM.Driver
 {
@@ -13,10 +14,11 @@ namespace Uber.BLL.ModelVM.Driver
 
         public Vehicle? Vehicle { get; set; }
         public string Name { get; set; }
-
+        public Gender Gender { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
 
+        public DateTime DateOfBirth { get; set; }
         public IFormFile? File { get; set; }
         public string? ImagePath { get; set; }
     }
